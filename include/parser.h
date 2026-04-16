37c17075f8ba5b795730b6dd9d73954db97fdd38
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-16 14:46:06 +0530

    Update parser.h

--- include/parser.h
+++ include/parser.h
@@ -1,22 +1,27 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdio.h>
 #include "lexer.h"
 
-// AST Node vagaigal
+// AST Node vagaigal (Types of nodes in our tree)
 typedef enum {
-    NODE_VAR_DECL,  // Variable declaration (எண் அ = 100)
-    NODE_PRINT,     // Output (கூறு)
-    NODE_BINARY_OP  // Math operations (+, -, *, /)
+    NODE_VAR_DECL,   // Variable declaration (e.g., எண் i = 0)
+    NODE_PRINT,      // Output (e.g., கூறு)
+    NODE_BINARY_OP,  // Math operations (+, -, *, /)
+    NODE_LOOP        // Loop logic (e.g., சு)
 } NodeType;
 
+// Abstract Syntax Tree Structure
 typedef struct ASTNode {
-    NodeType type;
-    Token token;
-    struct ASTNode *left;
-    struct ASTNode *right;
+    NodeType type;          // Node-oda vagai
+    Token token;            // Intha node-kulla irukira token (value/name)
+    struct ASTNode *left;   // Idathu pakkam branch
+    struct ASTNode *right;  // Valathu pakkam branch
 } ASTNode;
 
-void parse(FILE *file);
+// Function declarations
+void parse(FILE *file);                      // Parser-a start panna
+ASTNode* create_node(NodeType type, Token t); // Puthu node create panna
 
 #endif
