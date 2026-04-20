d5615100287410b3c817740357bc4b7f2ac300ee
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-20 14:59:57 +0530

    Update codegen.h

--- include/codegen.h
+++ include/codegen.h
@@ -1,12 +1,19 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
+
 #include <llvm-c/Core.h>
 
-
+// கம்பைலர் தொடக்க நிலைகள்
 void tamizhi_codegen_init();
 void tamizhi_generate_entry();
 void tamizhi_codegen_finish();
+
+// வேரியபிள் மற்றும் லூப் லாஜிக்
 void tamizhi_gen_var_decl(char* name, int initial_value);
 void tamizhi_gen_loop_test(int limit);
+
+// --- புதிய பங்க்ஷன்: கூட்டல் மற்றும் பிரிண்ட் செய்ய ---
+// இந்த வரி இல்லை என்றால் 'parser.c' ஆல் இதைப் பயன்படுத்த முடியாது
+void tamizhi_gen_add_and_print(int n1, int n2); 
 
 #endif
