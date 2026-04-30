55c1c553b98e3ddd35d8aa9908813abb1a56b510
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-04-30 14:20:19 +0530

    Update decoder.c

--- core/decoder.c
+++ core/decoder.c
@@ -1,1 +1,33 @@
-.
+#include <stdio.h>
+
+int dna_to_pair(char dna) {
+    switch (dna) {
+        case 'A': return 0;
+        case 'C': return 1;
+        case 'G': return 2;
+        case 'T': return 3;
+        default: return 0;
+    }
+}
+
+void decode_logic(const char* dna_path, const char* output_path) {
+    FILE *fin = fopen(dna_path, "r");
+    FILE *fout = fopen(output_path, "wb");
+    if (!fin || !fout) { printf("Error opening files!\n"); return; }
+
+    int dna_chars[4];
+    while ((dna_chars[0] = fgetc(fin)) != EOF) {
+        dna_chars[1] = fgetc(fin);
+        dna_chars[2] = fgetc(fin);
+        dna_chars[3] = fgetc(fin);
+
+        unsigned char ch = 0;
+        ch |= (dna_to_pair(dna_chars[0]) << 6);
+        ch |= (dna_to_pair(dna_chars[1]) << 4);
+        ch |= (dna_to_pair(dna_chars[2]) << 2);
+        ch |= (dna_to_pair(dna_chars[3]));
+        fputc(ch, fout);
+    }
+    fclose(fin); fclose(fout);
+    printf("வெற்றி! அசல் கோப்பு மீட்டெடுக்கப்பட்டது.\n");
+}
